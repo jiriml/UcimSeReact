ce05cca6555d05922f12ec7e40ba6e32d171a7d4
Jiří <machjiri17@gmail.com>
2025-08-04 10:22:04 +0200

    První test Reactu

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Ahoj toto je test</h1>
+      <p>číslo 1</p>
+    </>
+  )
+}
+
+export default App
